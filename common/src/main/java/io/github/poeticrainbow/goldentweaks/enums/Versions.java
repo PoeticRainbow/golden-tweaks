74245514ae9243ca0029d61c3d9935cd2fc69281
PoeticRainbow <jarrett.bayer@outlook.com>
2026-03-23 00:05:09 -0400

    add allowed values to enumtweaks

--- common/src/main/java/io/github/poeticrainbow/goldentweaks/enums/Versions.java
+++ common/src/main/java/io/github/poeticrainbow/goldentweaks/enums/Versions.java
@@ -1,10 +1,17 @@
 package io.github.poeticrainbow.goldentweaks.enums;
 
+import java.util.List;
+
 public enum Versions {
+    CLASSIC,
+    INDEV,
+    INFDEV,
     ALPHA,
     BETA,
     RELEASE,
     MODERN;
+
+    public static final List<Versions> MAIN_VERSIONS = List.of(ALPHA, BETA, RELEASE, MODERN);
 
     @Override
     public String toString() {
--- common/src/main/java/io/github/poeticrainbow/goldentweaks/tweak/types/EnumTweak.java
+++ common/src/main/java/io/github/poeticrainbow/goldentweaks/tweak/types/EnumTweak.java
@@ -1,6 +1,8 @@
 package io.github.poeticrainbow.goldentweaks.tweak.types;
 
+import io.github.poeticrainbow.goldentweaks.GoldenTweaks;
 import io.github.poeticrainbow.goldentweaks.tweak.Tweak;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
 
@@ -8,8 +10,29 @@ public class EnumTweak<T extends Enum<T>> extends Tweak<Enum<T>> {
     private final List<T> values;
 
     public EnumTweak(String key, Enum<T> defaultValue) {
+        this(key, defaultValue, defaultValue.getDeclaringClass().getEnumConstants());
+    }
+
+    public EnumTweak(String key, Enum<T> defaultValue, T[] allowedValues) {
+        this(key, defaultValue, List.of(allowedValues));
+    }
+
+    public EnumTweak(String key, Enum<T> defaultValue, List<T> allowedValues) {
         super(key, defaultValue);
-        this.values = List.of(defaultValue.getDeclaringClass().getEnumConstants());
+        this.values = allowedValues;
+    }
+
+    @Override
+    public void set(Enum<T> value) {
+        if (values.contains(value)) {
+            super.set(value);
+        } else {
+            GoldenTweaks.LOGGER.error(
+                "Tried to set tweak to value {} which is not allowed (allowed: {})",
+                value,
+                StringUtils.join(values, ", ")
+            );
+        }
     }
 
     public void next() {
